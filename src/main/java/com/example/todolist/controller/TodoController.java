ec4d672e2ed12382a5edbc1c712045d8c22dd60d
TORIBDA <david.moses.toribio@oocl.com>
2021-08-19 14:21:12 +0800

    [TodoList][TORIBDA] refactor: changed root to single slash

--- src/main/java/com/example/todolist/controller/TodoController.java
+++ src/main/java/com/example/todolist/controller/TodoController.java
@@ -3,20 +3,18 @@ package com.example.todolist.controller;
 import com.example.todolist.entity.Todo;
 import com.example.todolist.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/todos")
+@RequestMapping("/")
 public class TodoController {
 
     @Autowired
     private TodoService todoService;
 
-    @GetMapping
+    @GetMapping("/todos")
     public List<Todo> getAllTodo() {
         return todoService.getAllTodo();
     }
